dfdc781fba3590e7ab7a77f753027dacba26e5e3
sudheesh001 <sudheesh1995@outlook.com>
2015-04-25 06:12:54 +0530

    Adding basic barebone application start

---
+++ server.py
@@ -0,0 +1,60 @@
+# all the imports
+import os,binascii
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+		render_template, flash
+from flaskext.mysql import MySQL
+from flask_mail import Mail,Message
+from config import config, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+import datetime
+ 
+import logging
+from logging.handlers import SMTPHandler
+credentials = None
+
+mysql = MySQL()
+# create our little application :)
+
+app = Flask(__name__)
+
+for key in config:
+	app.config[key] = config[key]
+
+mail = Mail(app)
+# Mail
+mail.init_app(app)
+
+if MAIL_USERNAME or MAIL_PASSWORD:
+	credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'resetpass', credentials)
+	mail_handler.setLevel(logging.ERROR)
+	app.logger.addHandler(mail_handler)
+
+mysql.init_app(app)
+app.config.from_object(__name__)
+
+def tup2float(tup):
+	return float('.'.join(str(x) for x in tup))
+
+def get_cursor():
+	return mysql.connect().cursor()
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.route('/')
+def screen():
+	return render_template('index.html')
+
+@app.teardown_appcontext
+def close_db():
+	"""Closes the database again at the end of the request."""
+	get_cursor().close()
+
+if __name__ == '__main__':
+	app.debug = True
+	app.secret_key=os.urandom(24)
+	# app.permanent_session_lifetime = datetime.timedelta(seconds=200)
+	app.run()
