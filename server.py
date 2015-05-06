56b03d2943b009f8c6aafc457ae2ef8adc1834da
sudheesh001 <sudheesh1995@outlook.com>
2015-05-06 21:28:54 +0530

    Profile edits and DB registration and login

--- server.py
+++ server.py
@@ -69,8 +69,7 @@ def register():
 			if password == confpassword:
 				insertQuery = 'insert into Authentication VALUES ("%s","%s","%s","%s","%s","%s")'
 				db.execute(insertQuery%(index, username, password, usertype, verified, lastRecovery))
-				db.execute("COMMIT")
-				userInsertQuery = 'insert into Users (`index`) VALUES ("%s")'%index
+				userInsertQuery = 'insert into Users (index,username) VALUES ("%s","%s")'%(index,username)
 				db.execute(userInsertQuery)
 				db.execute("COMMIT")
 				print "success"
@@ -83,12 +82,40 @@ def register():
 			return redirect(url_for('register'))
 	return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+	global store
+	error = None
+	db = get_cursor()
+	session['temp'] = 0
+	if request.method == 'POST':
+		uname = str(request.form['username'])
+		password = str(request.form['password'])
+		sql = 'select count(*) from Authentication where username="%s" AND password="%s"'%(uname, password)
+		db.execute(sql)
+		result = db.fetchone()[0]
+		if not result:
+			error = "Invalid Username / Password"
+		else:
+			session['logged_in'] = True
+			sql = 'select userType from Authentication where username="%s" AND password="%s"'%(uname, password)
+			db.execute(sql)
+			result = db.fetchone()[0]
+			session['temp'] = result
+			sql = 'select `index` from Authentication where username="%s" AND password="%s"'%(uname, password)
+			db.execute(sql)
+			uid = db.fetchone()[0]
+			db.execute("COMMIT")
+			app.config['USERNAME'] = uname
+			app.config['USERID'] = uid
+			return redirect(url_for('editprofile'))
 	return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
+	index = app.config['USERID']
+	# Logged in  Index value
+	print 'Dashboard: '+index
 	return render_template('dashboard.html')
 
 @app.route('/enterprise')
@@ -125,14 +152,49 @@ def invoices():
 def profile():
 	return render_template('profile.html')
 
-@app.route('/editprofile')
+@app.route('/editprofile', methods=['GET', 'POST'])
 def editprofile():
+	db = get_cursor()
+	index = app.config['USERID']
+	userNameQuery = 'select * from Authentication where username="%s"'%app.config['USERNAME']
+	db.execute(userNameQuery)
+	userData = db.fetchone()[0]
+	print userData
+	if request.method == 'POST':
+		fname = request.form['fname']
+		lname = request.form['lname']
+		uemail = request.form['email']
+		phno = request.form['phoneno']
+		pancard = request.form['pancard']
+		aadharcard = request.form['aadharcard']
+		companyAff = request.form['companyAffiliation']
+		currentQty = request.form['currentQuantity']
+		gasAgencyAff = request.form['gasCompanyAffiliation']
+		addr1 = request.form['addressLineOne']
+		addr2 = request.form['addressLineTwo']
+		addr3 = request.form['city']
+		addr4 = request.form['state']
+		addr5 = request.form['zipcode']
+		country = request.form['country']
+		addr = addr1 + '|' + addr2 + '|' + addr3 + '|' + addr4 + '|' + addr5 + '|' + country
+		sql = 'update Users set firstName="%s", lastName="%s", userEmail="%s", addressDetails="%s", aadharCardNo="%s", PANCardNo="%s", companyAffiliation="%s", currentQuantity="%s", gasAgencyAffiliation="%s", phoneNumber="%s" where username="%s"'
+		db.execute(sql%(fname, lname, uemail, addr, aadharcard, pancard, companyAff, currentQty, gasAgencyAff, phno, app.config['USERNAME']))
+		db.execute("COMMIT")
+		return redirect(url_for('dashboard'))
 	return render_template('editprofile.html')
 
 @app.teardown_appcontext
 def close_db():
 	"""Closes the database again at the end of the request."""
 	get_cursor().close()
+
+@app.route('/logout')
+def logout():
+	if session['logged_in'] != None:
+		if session['logged_in']==True:
+			session.pop('logged_in', None)
+			session.pop('temp',0)
+	return redirect(url_for('login'))#show_entries.html
 
 if __name__ == '__main__':
 	app.debug = True
